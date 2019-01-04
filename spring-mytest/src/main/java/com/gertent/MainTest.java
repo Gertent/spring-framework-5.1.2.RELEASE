6752c06c4f994aa6691d951b072f6f0665dd8532
wangyunfu <Abc123456>
2019-01-04 13:43:48 +0800

    测试

--- spring-mytest/src/main/java/com/gertent/MainTest.java
+++ spring-mytest/src/main/java/com/gertent/MainTest.java
@@ -4,6 +4,7 @@ import com.gertent.bean.Person;
 import com.gertent.config.MainConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Author wyf
@@ -14,13 +15,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class MainTest {
     public static void main(String[] args) {
 
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-//        Person bean = (Person) applicationContext.getBean("person");
-//        System.out.println(bean);
-
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
-        Person bean = applicationContext.getBean(Person.class);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Person bean = (Person) applicationContext.getBean("person");
         System.out.println(bean);
+
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+//        Person bean = applicationContext.getBean(Person.class);
+//        System.out.println(bean);
 
         String[] namesForType = applicationContext.getBeanNamesForType(Person.class);
         for (String name : namesForType) {
