eb6217c0a3a5f9b5045f53f14ebcc2e7c8ed7c7b
wangyunfu <Abc123456>
2019-03-04 14:59:59 +0800

    测试

--- spring-mytest/src/main/java/com/gertent/condition/MyImportSelector.java
+++ spring-mytest/src/main/java/com/gertent/condition/MyImportSelector.java
@@ -3,14 +3,12 @@ package com.gertent.condition;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-/**
- * @Author wyf
- * @Date 2019/1/2 14:35
- * @Desc
- * @Version 1.0
- */
+//自定义逻辑返回需要导入的组件
 public class MyImportSelector implements ImportSelector {
+	//返回值，就是导入到容器中的组件全类名
+	//AnnotationMetadata：当前标注@Import注解的类的所有注解信息
     public String[] selectImports(AnnotationMetadata annotationMetadata) {
+    	//方法不要返回null值
         return new String[]{"com.gertent.bean.Blue","com.gertent.bean.Yellow"};
     }
 }
